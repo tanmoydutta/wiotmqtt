747f51a7db72ef718afecc93e424bfeef23a3c7c
tanmoy.dutta <tanmoy.dutta@nl.ibm.com>
2022-02-25 20:46:41 +0100

    Updated

--- mqttClientIBMCloudIoT.py
+++ mqttClientIBMCloudIoT.py
@@ -1,22 +1,24 @@
-# d:2wy0zx:dropper:drop
-#
-
+# Author : Tanmoy Dutta
+# Email : tanmoy.dutta@gmail.
+# Description : This code below shows how to connect to Watson IoT
+# MQTT server thorugh secure connection and publish data.
 
 import time
 import paho.mqtt.client as paho
 import ssl
 
+#MQTT Connection configuration
 org = "<Org Id of the Watson Iot Platform>"
 deviceType = "<Device Type>"
 deviceId = "<Device Id as defined at the time of device registration>"
 authUser = "use-token-auth" #default user id for authentication
 authToken = "<Authentication Token>"
-mqttTopic = "iot-2/evt/status/fmt/json"
+mqttTopic = "iot-2/evt/status/fmt/json" #Data being published using this topic in JSON format
 mqttHostFragment = ".messaging.internetofthings.ibmcloud.com"
 mqttHost = org + mqttHostFragment
 mqttClientId = "d:" + org + ":" + deviceType + ":" + deviceId
 mqttPort = 8883 #Secure Port
-sampleData = "{\"d\":{\"a\":8}}"
+sampleData = "{\"d\":{\"a\":8}}" #Sample data
 
 #define callbacks
 def on_message(client, userdata, message):
@@ -46,14 +48,4 @@ client.connect(mqttHost, mqttPort, 60)
 client.loop_start()
 #wait to allow publish and logging and exit
 time.sleep(1)
-#client.publish("iot-2/evt/status/fmt/json","{\"d\":{\"a\":1}}",0)
-#time.sleep(1)
-#client.publish("iot-2/evt/status/fmt/json","{\"d\":{\"a\":5}}",0)
-#time.sleep(1)
-#client.publish("iot-2/evt/status/fmt/json","{\"d\":{\"a\":3}}",0)
-#time.sleep(1)
-#client.publish("iot-2/evt/status/fmt/json","{\"d\":{\"a\":6}}",0)
-#time.sleep(1)
-#client.publish("iot-2/evt/status/fmt/json","{\"d\":{\"a\":7}}",0)
-#time.sleep(1)
 client.disconnect()
